405234808aed65f80297b65f4543696bb77640c7
Nonsenj <oilchattong@gmail.com>
2024-08-05 04:57:55 +0700

    update setup

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from sqlmodel import Field, SQLModel, create_engine, Session, select
 
-class Item(BaseModel):
+class BaseItem(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     name: str
     description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+    price: float = 0.14
+    tax: float | None = None
 
 app = FastAPI()
 
@@ -19,5 +22,5 @@ async def read_time(item_id: int):
     return {"Item_id": item_id}
 
 @app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item}
+async def create_item(item_id: int, item: BaseItem):
+    return {"item_id": item_id, **item.model_dump()}
