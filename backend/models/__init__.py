3e2b999d5dc476d49863b2b6225e20070109dd42
Nonsenj <oilchattong@gmail.com>
2024-08-28 02:52:48 +0700

    Edit model

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import AsyncIterable
 from ..config import Settings
 
 from sqlmodel import Field, SQLModel, create_engine, Session, select
@@ -33,12 +33,18 @@ def init_db(settings: Settings):
         connect_args= connect_args,
     )
 
-async def create_all():
+async def recreate_table():
     async with engine.begin() as conn:
-        #await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncIterable[AsyncSession]:
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
         yield session
+
+async def close_session():
+    global engine
+    if engine is None:
+        raise Exception()
+    await engine.dispose()
